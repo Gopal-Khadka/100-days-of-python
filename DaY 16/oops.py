ca590f7d13091ba3d22262fe52bc7227e800a2b6
Gopal Khadka <117714059+Gopal-Khadka@users.noreply.github.com>
2022-12-15 21:57:06 +0545

    Add files via upload

---
+++ DaY 16/oops.py
@@ -0,0 +1,23 @@
+# attribute and methods make objects .
+# basically what it has and what it does
+# attributes is just a fancy word for variables.
+# methods are functions of certain objects.
+# in oops we make real life objects by combining data nad functionality.
+# classes are like blueprint for objects . We can create objects using classes.
+
+
+# for eg:
+# car = CarBluePrint()
+# car is object and CarBluePrint is class
+
+# Notice that each word first letter is capitalized as convention.[pascal case
+
+
+from turtle import *
+timmy= Turtle()
+print(timmy)
+my_screen=Screen()
+print(my_screen.canvheight)
+# canvheight is a method
+
+my_screen.exitonclick()
